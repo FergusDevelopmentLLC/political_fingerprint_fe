fc2eb0015b21d67149e9a79b8c1298959e498f3e
Will Carter <will.carter@fergusllc.com>
2020-08-28 11:22:39 -0600

    add node

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express()
+const dir = __dirname + '/8values_fe/public/'
+console.log('dir', dir)
+app.use(express.static(dir)) //allows serving of static files in public folder
+
+const server = app.listen(8686, () => {
+  console.log('App listening at port %s', server.address().port)
+})
+
+//visit http://127.0.0.1:8686/index.html
+//index is in /8values_fe/public/ folder.
