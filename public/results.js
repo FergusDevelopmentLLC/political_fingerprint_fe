802d91542ec7a0d7eeda01b0344ef9e9b34e75ac
Will Carter <will.carter@fergusllc.com>
2020-09-24 13:48:40 -0600

    add static d3 map

--- public/results.js
+++ public/results.js
@@ -39,24 +39,47 @@ peace = getQueryVariable("d")
 liberty = getQueryVariable("g")
 progress = getQueryVariable("s")
 
+let version = parseInt(getQueryVariable("v"))
+
 wealth = (100 - equality).toFixed(1)
-might = (100 - peace).toFixed(1)
+
+if(version === 1) {
+  might = (100 - peace).toFixed(1)
+}
+
 authority = (100 - liberty).toFixed(1)
-tradition = (100 - progress).toFixed(1)
+
+if(version === 1) {
+  tradition = (100 - progress).toFixed(1)
+}
 
 setBarValue("equality", equality)
 setBarValue("wealth", wealth)
-setBarValue("peace", peace)
-setBarValue("might", might)
+
+if(version === 1) {
+  setBarValue("peace", peace)
+  setBarValue("might", might)
+}
+
 setBarValue("liberty", liberty)
 setBarValue("authority", authority)
-setBarValue("progress", progress)
-setBarValue("tradition", tradition)
+
+if(version === 1) {
+  setBarValue("progress", progress)
+  setBarValue("tradition", tradition)
+}
 
 document.getElementById("economic-label").innerHTML = setLabel(equality, econArray)
-document.getElementById("diplomatic-label").innerHTML = setLabel(peace, diplArray)
+
+if(version === 1) {
+  document.getElementById("diplomatic-label").innerHTML = setLabel(peace, diplArray)
+}
+
 document.getElementById("state-label").innerHTML = setLabel(liberty, govtArray)
-document.getElementById("society-label").innerHTML = setLabel(progress, sctyArray)
+
+if(version === 1) {
+  document.getElementById("society-label").innerHTML = setLabel(progress, sctyArray)
+}
 
 ideology = ""
 ideodist = Infinity
@@ -64,8 +87,12 @@ for (var i = 0; i < ideologies.length; i++) {
   dist = 0
   dist += Math.pow(Math.abs(ideologies[i].stats.econ - equality), 2)
   dist += Math.pow(Math.abs(ideologies[i].stats.govt - liberty), 2)
-  dist += Math.pow(Math.abs(ideologies[i].stats.dipl - peace), 1.73856063)
-  dist += Math.pow(Math.abs(ideologies[i].stats.scty - progress), 1.73856063)
+
+  if(version === 1) {
+    dist += Math.pow(Math.abs(ideologies[i].stats.dipl - peace), 1.73856063)
+    dist += Math.pow(Math.abs(ideologies[i].stats.scty - progress), 1.73856063)
+  }
+
   if (dist < ideodist) {
     ideology = ideologies[i].name
     ideodist = dist
@@ -93,40 +120,56 @@ window.onload = function () {
   ctx.drawImage(img, 20, 170, 100, 100);
   img = document.getElementById("img-wealth")
   ctx.drawImage(img, 680, 170, 100, 100)
-  img = document.getElementById("img-might")
-  ctx.drawImage(img, 20, 290, 100, 100)
-  img = document.getElementById("img-peace")
-  ctx.drawImage(img, 680, 290, 100, 100)
+
+  if(version === 1) {
+    img = document.getElementById("img-might")
+    ctx.drawImage(img, 20, 290, 100, 100)
+    img = document.getElementById("img-peace")
+    ctx.drawImage(img, 680, 290, 100, 100)
+  }
+
   img = document.getElementById("img-liberty")
   ctx.drawImage(img, 20, 410, 100, 100)
   img = document.getElementById("img-authority")
   ctx.drawImage(img, 680, 410, 100, 100)
-  img = document.getElementById("img-tradition")
-  ctx.drawImage(img, 20, 530, 100, 100)
-  img = document.getElementById("img-progress")
-  ctx.drawImage(img, 680, 530, 100, 100)
+  
+  if(version === 1) {
+    img = document.getElementById("img-tradition")
+    ctx.drawImage(img, 20, 530, 100, 100)
+    img = document.getElementById("img-progress")
+    ctx.drawImage(img, 680, 530, 100, 100)
+  }
 
   ctx.fillStyle = "#222222"
   ctx.fillRect(120, 180, 560, 80)
   ctx.fillRect(120, 300, 560, 80)
   ctx.fillRect(120, 420, 560, 80)
   ctx.fillRect(120, 540, 560, 80)
+  
   ctx.fillStyle = "#f44336"
   ctx.fillRect(120, 184, 5.6 * equality - 2, 72)
   ctx.fillStyle = "#00897b"
   ctx.fillRect(682 - 5.6 * wealth, 184, 5.6 * wealth - 2, 72)
-  ctx.fillStyle = "#ff9800"
-  ctx.fillRect(120, 304, 5.6 * might - 2, 72)
-  ctx.fillStyle = "#03a9f4"
-  ctx.fillRect(682 - 5.6 * peace, 304, 5.6 * peace - 2, 72)
+  
+  if(version === 1) {
+    ctx.fillStyle = "#ff9800"
+    ctx.fillRect(120, 304, 5.6 * might - 2, 72)
+    ctx.fillStyle = "#03a9f4"
+    ctx.fillRect(682 - 5.6 * peace, 304, 5.6 * peace - 2, 72)
+  }
+
   ctx.fillStyle = "#ffeb3b"
   ctx.fillRect(120, 424, 5.6 * liberty - 2, 72)
   ctx.fillStyle = "#3f51b5"
   ctx.fillRect(682 - 5.6 * authority, 424, 5.6 * authority - 2, 72)
-  ctx.fillStyle = "#8bc34a"
-  ctx.fillRect(120, 544, 5.6 * tradition - 2, 72)
-  ctx.fillStyle = "#9c27b0"
-  ctx.fillRect(682 - 5.6 * progress, 544, 5.6 * progress - 2, 72)
+  
+  if(version === 1) {
+    ctx.fillStyle = "#8bc34a"
+    ctx.fillRect(120, 544, 5.6 * tradition - 2, 72)
+    ctx.fillStyle = "#9c27b0"
+    ctx.fillRect(682 - 5.6 * progress, 544, 5.6 * progress - 2, 72)
+  }
+
   ctx.fillStyle = "#222222"
   ctx.font = "700 80px Montserrat"
   ctx.textAlign = "left"
@@ -135,24 +178,52 @@ window.onload = function () {
   ctx.fillText(ideology, 20, 140)
 
   ctx.textAlign = "left"
+  
   if (equality > 30) { ctx.fillText(equality + "%", 130, 237.5) }
-  if (might > 30) { ctx.fillText(might + "%", 130, 357.5) }
+  
+  if(version === 1) {
+    if (might > 30) { ctx.fillText(might + "%", 130, 357.5) }
+  }
+
   if (liberty > 30) { ctx.fillText(liberty + "%", 130, 477.5) }
-  if (tradition > 30) { ctx.fillText(tradition + "%", 130, 597.5) }
+  
+  if(version === 1) {
+    if (tradition > 30) { ctx.fillText(tradition + "%", 130, 597.5) }
+  }
+
   ctx.textAlign = "right"
   if (wealth > 30) { ctx.fillText(wealth + "%", 670, 237.5) }
-  if (peace > 30) { ctx.fillText(peace + "%", 670, 357.5) }
-  if (authority > 30) { ctx.fillText(authority + "%", 670, 477.5) }
-  if (progress > 30) { ctx.fillText(progress + "%", 670, 597.5) }
 
+  if(version === 1) {
+    if (peace > 30) { ctx.fillText(peace + "%", 670, 357.5) }
+  }
+
+  if (authority > 30) { ctx.fillText(authority + "%", 670, 477.5) }
+  
+  if(version === 1) {
+    if (progress > 30) { ctx.fillText(progress + "%", 670, 597.5) }
+  }
+  
   ctx.font = "300 30px Montserrat"
   ctx.fillText("8values.github.io", 780, 60)
   ctx.fillText(version, 780, 90)
   ctx.textAlign = "center"
   ctx.fillText("Economic Axis: " + document.getElementById("economic-label").innerHTML, 400, 175)
-  ctx.fillText("Diplomatic Axis: " + document.getElementById("diplomatic-label").innerHTML, 400, 295)
+  
+  if(version === 1) {
+    ctx.fillText("Diplomatic Axis: " + document.getElementById("diplomatic-label").innerHTML, 400, 295)
+  }
+
   ctx.fillText("Civil Axis: " + document.getElementById("state-label").innerHTML, 400, 415)
-  ctx.fillText("Societal Axis: " + document.getElementById("society-label").innerHTML, 400, 535)
+  
+  if(version === 1) {
+    ctx.fillText("Societal Axis: " + document.getElementById("society-label").innerHTML, 400, 535)
+  }
+
+  if(version === 2) {
+    document.getElementById('results-diplomatic').style.display = 'none'
+    document.getElementById('results-societal').style.display = 'none'
+  }
 
   // document.getElementById("banner").src = c.toDataURL();
 }
